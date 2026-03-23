d14d1659e2899c3660fe6c37a56168e84d8bf71c
Jacob Gustafson <7557867+Poikilos@users.noreply.github.com>
2026-03-22 21:09:09 -0400

    Update documentation URL.

--- kc/nodes.lua
+++ kc/nodes.lua
@@ -1,4 +1,4 @@
--- tiles: +Y, -Y, +X, -X, +Z, -Z. In English: top, bottom, right, left, back, front.  - http://dev.minetest.net/minetest.register_node
+-- tiles: +Y, -Y, +X, -X, +Z, -Z. In English: top, bottom, right, left, back, front.  - https://github.com/luanti-org/luanti/blob/master/doc/lua_api.md#registered-definitions
 minetest.register_node("kc:tooling_station", {
 	description = "Tooling Station",
 	tiles = {"kc_tooling_station_top.png", "kc_tooling_station_bottom.png", "kc_tooling_station_sides.png",
@@ -8,3 +8,4 @@ minetest.register_node("kc:tooling_station", {
 	groups = {choppy = 3, oddly_breakable_by_hand = 2, flammable = 3},
 	sounds = default.node_sound_wood_defaults(),
 })
+
